37a8dafca8233fbe6e453cc13c565f874af2ff02
Paul Spears <paul.spears@oasisdigital.com>
2018-09-30 08:03:37 -0500

    curriculum update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,8 @@ const routes: Routes = [
 
 const config: ExtraOptions = {
   useHash: true,
-  enableTracing: false // Turn this on to log routing events to the console
+  enableTracing: false, // Turn this on to log routing events to the console
+  scrollPositionRestoration: 'enabled'
   // ,preloadingStrategy: PreloadAllModules
 };
 
